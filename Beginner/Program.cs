f93f2d020505c5f670970959732c8ccee370f6e0
Hussain Aminu <hussain_aminu@yahoo.com>
2023-03-23 10:53:22 +0300

    hello

--- Beginner/Program.cs
+++ Beginner/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace Beginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var name = "Aminu";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven");
+                    break;
+
+                }
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
